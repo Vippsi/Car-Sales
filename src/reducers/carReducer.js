354b0e3e54f18371bb40a612e6a732db32b03278
Jonathan <jonathanthornton1997@gmail.com>
2020-06-09 15:22:31 -0700

    Finished MVP

---
+++ src/reducers/carReducer.js
@@ -0,0 +1,50 @@
+import { ADD_FEATURE } from "../actions/carActions"
+import { REMOVE_FEATURE } from "../actions/carActions"
+
+const initialState = {
+    additionalPrice: 0,
+    car: {
+      price: 26395,
+      name: '2019 Ford Mustang',
+      image:
+        'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
+      features: []
+    },
+    additionalFeatures: [
+      { id: 1, name: 'V-6 engine', price: 1500 },
+      { id: 2, name: 'Racing detail package', price: 1500 },
+      { id: 3, name: 'Premium sound system', price: 500 },
+      { id: 4, name: 'Rear spoiler', price: 250 }
+    ]
+}
+
+export const carReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case ADD_FEATURE:
+            const newPrice = state.additionalPrice + action.payload.price
+            console.log(state.additionalPrice)
+            return {
+                ...state,
+                additionalPrice: newPrice,
+                car:{
+                    ...state.car,
+                    
+                    features:[...state.car.features, action.payload]},
+                
+            }
+            case REMOVE_FEATURE:
+                const updatedPrice = state.additionalPrice - action.payload.price
+                return{
+                    ...state,
+                    additionalPrice: updatedPrice,
+                    car:{
+                        ...state.car,
+                        features:[
+                            ...state.car.features.filter((mod) => mod.id !== action.payload.id)
+                        ]
+                    }
+                }
+        default:
+            return state
+    }
+}
